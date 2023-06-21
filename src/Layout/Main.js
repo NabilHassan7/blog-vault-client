a28941e84183bcd5fadf5d3c29d32c3057f869f3
NabilHassan7 <fakrulibnehassan@gmail.com>
2023-06-21 21:55:09 +0600

    basic structure started

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
-            <Outlet></Outlet>
+            {/*  Initial Outlet  */}
+            <Container>
+                <Row>
+                    {/* Space for left navbar */}
+                    <Col lg='2'>
+                        <h2>Left navbar</h2>
+                    </Col>
+
+                    {/* Space for outlet - Outlet will change along with the content */}
+                    <Col lg='7'>
+                        {/* Outlet window */}
+                        <Outlet>
+                            <h2>This is the outlet window</h2>
+                        </Outlet>
+                    </Col>
+
+                    {/* Space for right sidenav */}
+                    <Col lg='3'>
+                        <h2>Right navbar</h2>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };

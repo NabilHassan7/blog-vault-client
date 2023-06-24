6dfe27ce7a90490a130b26a14575f7b7186198d8
NabilHassan7 <fakrulibnehassan@gmail.com>
2023-06-24 16:38:15 +0600

    work progress on left nav bar

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
+
+    // Loading news categories from server
+    const [newsCategories, setNewsCategories] = useState([]);
+
+    useEffect(() =>{
+        fetch('http://localhost:5000/news-categories')
+        .then(res => res.json())
+        .then(data => setNewsCategories(data));
+    },[])
+
     return (
         <div>
-            <h2>This is the left side nav</h2>
+            <h2>All news categories: {newsCategories.length}</h2>
+            {/* mapping to show the category list */}
+            {/* categories are linked dynamically */}
+            <div>
+                {
+                   newsCategories.map(newsCategory => <p key={newsCategory.id}>
+                        <Link to={`/category/${newsCategory.id}`}>{newsCategory.name}</Link> 
+                   </p>) 
+                }
+            </div>
         </div>
     );
 };

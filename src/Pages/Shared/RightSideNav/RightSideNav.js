a8b3281dd60a92be00fe52b041bbbcfaefb0f9e5
NabilHassan7 <fakrulibnehassan@gmail.com>
2023-06-24 16:49:22 +0600

    started work on right side nav, added react icons

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,10 +1,16 @@
 import React from 'react';
+// importing from react-bootstrap
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 const RightSideNav = () => {
     return (
-        <div>
-            <h2>This is the right side nav</h2>
-        </div>
+        <ButtonGroup vertical>
+            <Button variant="outline-primary">Login with Google</Button>
+            <Button variant="outline-dark">Login with Github</Button>
+            <Button variant="outline-secondary">Login with facebook</Button>
+            <Button variant="outline-success">Login with twiiter</Button>     
+        </ButtonGroup>
     );
 };
 
